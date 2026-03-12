e4673b5133ba90a33d0df508378e9a3317590d2a
DelffyngsGO <fcristiansilverazhang@gmail.com>
2026-03-12 14:31:18 -0300

    Update server.js

--- aula1-11-03-26/src/server.js
+++ aula1-11-03-26/src/server.js
@@ -2,8 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let alunos = [
+  {id: 1, nome:'Zhang', idade: 17},
+  {id: 2, nome:'Miguel', idade: 17},
+  {id: 3, nome:'Henrique', idade: 17},
+]
+
+let frutas = [
+  {name: "banana", price:5.00},
+  {name: "maçã", price:3.00},
+  {name: "tomate", price:2.50},
+]
+
+app.get('/frutas', (req, res) => {
+  res.json({
+    sucess: true,
+    data: frutas,
+  })
+})
+
 app.get('/', (req, res) => {
   res.send('Pai do Edubardo!')
+})
+
+app.get('/alunos', (req, res) => {
+  res.send().json({
+    success: true,
+    data: alunos,
+  })
 })
 
 app.listen(port, () => {
